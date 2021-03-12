0747f01295cf4d9530dc28236789f7285a85eb03
Unhookedx <tuccialessandrox@gmail.com>
2021-03-12 20:36:39 +0100

    usespring

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,6 +1,6 @@
 import { firebase } from "./../realtimedb/firebase";
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { Box } from "../components/Box";
 import { Svg } from "../components/Svg";
@@ -37,27 +37,22 @@ const Home = () => {
       value && setIsWindyToggle(value.isWindy);
     });
   }, []);
-  let x = useMotionValue(0);
-  const xInput = [-100, 0, 100];
-  const background = useTransform(x, xInput, [
+  let y = useMotionValue(0);
+  const yInput = [-100, 0, 100];
+  const spring = useSpring(y);
+  const background = useTransform(spring, yInput, [
     "linear-gradient(180deg, #ff5100 0%, rgb(211, 9, 40) 100%)",
     "linear-gradient(180deg, #7700ff 0%, rgb(68, 0, 255) 100%)",
     "linear-gradient(180deg, rgb(230, 255, 0) 0%, rgb(3, 209, 0) 100%)",
   ]);
-  useEffect(
-    () =>
-      x.onChange((latest) => {
-        console.log(latest);
-      }),
-    []
-  );
-  isWindyToggle ? x.set(80) : x.set(-90);
+
+  if (isWindyToggle !== undefined) {
+    isWindyToggle ? spring.set(80) : spring.set(-90);
+  }
   return (
     <Container style={{ background }}>
-      <Box
-      
-      >
-        <Svg x={x} xInput={xInput} />
+      <Box>
+        <Svg x={spring} xInput={yInput} />
       </Box>
       {/* <Text>CE VENTO</Text> */}
     </Container>
