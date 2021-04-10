0b745f46796991bf87189b81f134aa6bf25f1745
Unhookedx <tuccialessandrox@gmail.com>
2021-04-10 21:51:15 +0200

    test

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,7 +6,7 @@ import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
 import { StaleWhileRevalidate } from "workbox-strategies";
-import { firebase } from "./realtimedb/firebase";
+// import { firebase } from "./realtimedb/firebase";
 import "firebase/messaging";
 
 declare const self: ServiceWorkerGlobalScope;
