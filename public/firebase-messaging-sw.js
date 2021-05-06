a58340c6f74da8de66e573a261d6be63959d3f95
Unhookedx <tuccialessandrox@gmail.com>
2021-05-06 16:52:31 +0200

    sw fix

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,25 +1,25 @@
-importScripts("https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js");
+// importScripts("https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js");
 
-/** @type {ServiceWorkerGlobalScope} */
+// /** @type {ServiceWorkerGlobalScope} */
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBsPgk_lP39ZLMoMezAVZGM2WTc8e_bItU",
-  authDomain: "easykite-c750e.firebaseapp.com",
-  databaseURL:
-    "https://easykite-c750e-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "easykite-c750e",
-  storageBucket: "easykite-c750e.appspot.com",
-  messagingSenderId: "244819329697",
-  appId: "1:244819329697:web:31a918ac92f8cb5cae26e0",
-  measurementId: "G-4P713ZQ4M0",
-});
+// firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE,
+//   authDomain: "easykite-c750e.firebaseapp.com",
+//   databaseURL:
+//     "https://easykite-c750e-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "easykite-c750e",
+//   storageBucket: "easykite-c750e.appspot.com",
+//   messagingSenderId: "244819329697",
+//   appId: "1:244819329697:web:31a918ac92f8cb5cae26e0",
+//   measurementId: "G-4P713ZQ4M0",
+// });
 
-const messaging = firebase.messaging();
+// const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-});
+// messaging.onBackgroundMessage((payload) => {
+//   console.log(
+//     "[firebase-messaging-sw.js] Received background message ",
+//     payload
+//   );
+// });
