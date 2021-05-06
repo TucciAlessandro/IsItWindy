10279a2288e41826f2be2212f7513f8f0c16a587
Unhookedx <tuccialessandrox@gmail.com>
2021-05-06 16:47:40 +0200

    sw cloud messaging

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,10 @@ importScripts("https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.4.3/firebase-messaging.js");
 
 /** @type {ServiceWorkerGlobalScope} */
-// let self;
 
 firebase.initializeApp({
   apiKey: "AIzaSyBsPgk_lP39ZLMoMezAVZGM2WTc8e_bItU",
-  // apiKey: process.env.REACT_APP_FIREBASE,
   authDomain: "easykite-c750e.firebaseapp.com",
-  // authDomain: "easykite-c750e.firebaseapp.com",
   databaseURL:
     "https://easykite-c750e-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "easykite-c750e",
@@ -25,12 +22,4 @@ messaging.onBackgroundMessage((payload) => {
     "[firebase-messaging-sw.js] Received background message ",
     payload
   );
-  // Customize notification here
-  const notificationTitle = "Background Message Title";
-  const notificationOptions = {
-    body: "Background Message body.",
-    icon: "/firebase-logo.png",
-  };
-
-  // self.registration.showNotification(notificationTitle, notificationOptions);
 });
